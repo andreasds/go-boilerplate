91b44199b917002cfe3c3cba66038ecd56aec79c
Andreas <andreas.ds90@gmail.com>
2021-08-20 22:36:08 +0700

    repository using query to database (#7)
    * repository using query to database
    
    * break long line into multiple line

--- internal/infrastucture/database/mysql.go
+++ internal/infrastucture/database/mysql.go
@@ -3,6 +3,8 @@ package database
 import (
 	"fmt"
 
+	"github.com/andreasds/go-boilerplate/configs"
+	_ "github.com/go-sql-driver/mysql" // MySQL driver
 	"github.com/jmoiron/sqlx"
 	"github.com/rs/zerolog/log"
 )
@@ -12,13 +14,41 @@ const (
 	maxOpenConnection = 10
 )
 
-func CreateDBConnection(
+type MySQLConnection struct {
+	Read  *sqlx.DB
+	Write *sqlx.DB
+}
+
+func NewMySQLConnection(config *configs.Config) *MySQLConnection {
+	connection := new(MySQLConnection)
+	connection.Read = createDBConnection(
+		"read",
+		config.DB.MySQL.Read.User,
+		config.DB.MySQL.Read.Password,
+		config.DB.MySQL.Read.Host,
+		fmt.Sprint(config.DB.MySQL.Read.Port),
+		config.DB.MySQL.Read.Name,
+	)
+	connection.Write = createDBConnection(
+		"write",
+		config.DB.MySQL.Write.User,
+		config.DB.MySQL.Write.Password,
+		config.DB.MySQL.Write.Host,
+		fmt.Sprint(config.DB.MySQL.Write.Port),
+		config.DB.MySQL.Write.Name,
+	)
+
+	return connection
+}
+
+func createDBConnection(
+	method string,
 	username string,
 	password string,
 	host string,
 	port string,
 	dbName string,
-) (db *sqlx.DB, err error) {
+) (db *sqlx.DB) {
 	dataSourceName := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true",
 		username,
@@ -27,16 +57,18 @@ func CreateDBConnection(
 		port,
 		dbName,
 	)
-	db, err = sqlx.Connect("mysql", dataSourceName)
+	db, err := sqlx.Connect("mysql", dataSourceName)
 	if err != nil {
 		log.Fatal().
 			Err(err).
+			Str("method", method).
 			Str("host", host).
 			Str("port", port).
 			Str("dbName", dbName).
 			Msg("Failed to connect database")
 	} else {
 		log.Info().
+			Str("method", method).
 			Str("host", host).
 			Str("port", port).
 			Str("dbName", dbName).
